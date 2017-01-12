46056aca1ffa88db900fcc390300f831131731a6
Hoai Nguyen <tienhoai@lozi.vn>
2017-01-12 17:10:03 +0700

    Update the insert string.

--- src/searchAction/SearchResourcesAction.java
+++ src/searchAction/SearchResourcesAction.java
@@ -13,7 +13,9 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import searchAction.model.SearchResultElement;
 
 import static com.intellij.openapi.application.ApplicationManager.getApplication;
@@ -22,18 +24,22 @@ import static com.intellij.openapi.application.ApplicationManager.getApplication
  * Created by hoainguyen on 1/2/17.
  */
 public class SearchResourcesAction extends GotoActionBase implements DumbAware {
+
+    boolean isXmlFile = false;
+
     @Override
     protected void gotoActionPerformed(AnActionEvent e) {
         final Project project = e.getData(CommonDataKeys.PROJECT);
         if (project == null) return;
 
         FeatureUsageTracker.getInstance().triggerFeatureUsed("navigation.popup.file");
+        isXmlFile = isXmlFile(CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext()));
 
         final SearchFilterModel searchFilterModel = new SearchFilterModel(project);
         GotoActionCallback<SearchResultElement> callback = new GotoActionCallback<SearchResultElement>() {
             @Override
             protected ChooseByNameFilter<SearchResultElement> createFilter(@NotNull ChooseByNamePopup popup) {
-                return new SearchResourceByValueFilter(popup,searchFilterModel, SearchConfiguration.getInstance(project),project);
+                return new SearchResourceByValueFilter(popup, searchFilterModel, SearchConfiguration.getInstance(project), project);
             }
 
             @Override
@@ -54,12 +60,18 @@ public class SearchResourcesAction extends GotoActionBase implements DumbAware {
             if (editor != null) {
                 int offset = editor.getCaretModel().getOffset();
                 Document document = editor.getDocument();
-                String key = element.getName();
+                String key = isXmlFile ?
+                        "@" + element.getTag() + "/" + element.getName()
+                        : "R." + element.getTag() + "." + element.getName();
                 if (key != null) {
                     document.insertString(offset, key);
                     editor.getCaretModel().moveToOffset(offset + key.length());
                 }
             }
         }), "InsertResultToEditor", "", UndoConfirmationPolicy.DO_NOT_REQUEST_CONFIRMATION);
+    }
+
+    private static boolean isXmlFile(@Nullable VirtualFile file) {
+        return file != null && file.getName().endsWith(".xml");
     }
 }
